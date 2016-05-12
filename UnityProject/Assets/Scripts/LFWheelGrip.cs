764f63bbbc91450781c464b2a2ba38c4987f8db5
Ian <ian.elvar@gmail.com>
2016-05-12 23:23:36 +0100

    Add files via upload

--- UnityProject/Assets/Scripts/LFWheelGrip.cs
+++ UnityProject/Assets/Scripts/LFWheelGrip.cs
@@ -12,7 +12,7 @@ public class LFWheelGrip : MonoBehaviour {
     public float suspensionSpring = 2000;
     public float suspensionDamper = 200;
     public float steeringInput;
-    RaycastHit hit;
+    public float lateralGrip = 2000;
     #endregion
 
     #region public for the moment to show in Unity editor
@@ -20,12 +20,13 @@ public class LFWheelGrip : MonoBehaviour {
     public ConfigurableJoint susJoint;
     public GameObject contact;
     public Rigidbody contactRb;
-    public Vector3 lastWheelPosition;
+    public Vector3 lastHitPoint;
     public GameObject wheel;
     public Rigidbody rb;
     #endregion
 
     #region Internal private variables
+    RaycastHit hit;
     int layerMask;
     #endregion
 
@@ -51,10 +52,11 @@ public class LFWheelGrip : MonoBehaviour {
         {
             print("The GameObject this script is aplied to should be set to scale 1,1,1!!!!");
         }
-        
+
         // Needs to be an empty GO eventually - cube for convenience for now. Has to have Rigidybody for joint to attach to.
         contact = GameObject.CreatePrimitive(PrimitiveType.Cube);
         Destroy(gameObject.GetComponent("BoxCollider"));
+        //Destroy(gameObject.GetComponent("MeshRenderer"));
         contact.layer = 26;
         print(-wheel.transform.up);
         contact.transform.position = wheel.transform.position + -wheel.transform.up * (suspensionDistance + wheelRadius);
@@ -69,7 +71,7 @@ public class LFWheelGrip : MonoBehaviour {
         susJoint.autoConfigureConnectedAnchor = false;
         susJoint.secondaryAxis = new Vector3(0, 1, 0);
 
-        susJoint.xMotion = ConfigurableJointMotion.Free;
+        susJoint.xMotion = ConfigurableJointMotion.Limited;
         susJoint.yMotion = ConfigurableJointMotion.Limited;
         susJoint.zMotion = ConfigurableJointMotion.Free;
 
@@ -89,6 +91,14 @@ public class LFWheelGrip : MonoBehaviour {
         SJL.contactDistance = 0;
         susJoint.linearLimit = SJL;
 
+        //THIS CONTROLS LATERAL GRIP
+        var XD = new JointDrive(); //only XDrive used for now. X and Z can be used independently if we wish
+        XD.positionSpring = lateralGrip;
+        XD.positionDamper = 100;
+        XD.maximumForce = 10000000;
+        susJoint.xDrive = XD;
+
+        //THIS CONTROLS SUSPENSION SETTINGS
         var YD = new JointDrive(); //only yDrive used for now. X and Z can be used independently if we wish
         YD.positionSpring = suspensionSpring;
         YD.positionDamper = suspensionDamper;
@@ -118,29 +128,16 @@ public class LFWheelGrip : MonoBehaviour {
     {
         while(true)
         {
-            //if (Physics.Raycast(wheel.transform.position, -wheel.transform.up, out hit, rayDistance + radius, layerMask))
-            if(Physics.SphereCast(wheel.transform.position, wheelRadius,-wheel.transform.up, out hit, suspensionDistance - wheelRadius, layerMask))
+            if (Physics.Raycast(wheel.transform.position, -wheel.transform.up, out hit, suspensionDistance -.1f, layerMask))
+            //if(Physics.SphereCast(wheel.transform.position, wheelRadius,-wheel.transform.up, out hit, suspensionDistance - wheelRadius, layerMask))
             {
                 grounded = true;
                 contactRb.isKinematic = true;
 
-                Vector3 localToWheel = wheel.transform.InverseTransformPoint(hit.point);
-                
-                localToWheel.x = 0;
-                localToWheel.z = 0;
-                
-                Vector3 wheelPosition = wheel.transform.TransformPoint(localToWheel);
-                Debug.DrawLine(wheelPosition, wheelPosition + wheel.transform.up * 1, Color.blue, 1);
-                //print(hit.transform.gameObject.name);
-
-                //TODO: Add some trig code to determine the correct position.
-                //TODO: Plug in the grip code and generate an offset for the x position of the "contact" dummy.
-
-                contactRb.position = wheelPosition;
+                contactRb.position = hit.point;
                 contactRb.rotation = wheel.transform.rotation;
 
-                Debug.DrawLine(hit.point, hit.point + wheel.transform.up * 1, Color.red, 1);
-                lastWheelPosition = wheelPosition;
+                lastHitPoint = hit.point;
             }
             else
             {
