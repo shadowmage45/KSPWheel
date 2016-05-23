812fdf42beee91ef57a406faa6f2ff2bf7cd4d6d
shadowmage45 <shadowmage45@hotmail.com>
2016-05-23 12:26:17 -0600

    more work on stock-replacement PartModule, should be more-or-less functional, though still quite a bit to do

--- VSProject/KSPWheel/PartModule/KSPWheelModule.cs
+++ VSProject/KSPWheel/PartModule/KSPWheelModule.cs
@@ -5,31 +5,87 @@ namespace KSPWheel
 {
     /// <summary>
     /// A replacement for the stock wheel system that uses the KSPWheelCollider class for phsyics handling.
-    /// Intended to be a fully-function (if not fully-equivalent) replacement for the stock wheel modules and U5 WheelCollider component
+    /// Intended to be a fully-functional (but possibly not fully-equivalent) replacement for the stock wheel modules and U5 WheelCollider component
     /// </summary>
     public class KSPWheelModule : PartModule
     {
-
         #region REGION - Basic wheel parameters that cannot be pulled from the U5 WheelCollider component
+
+        /// <summary>
+        /// Name of the transform that the wheel collider component should be attached to/manipulate.
+        /// </summary>
         [KSPField]
         public string wheelColliderName;
+
+        /// <summary>
+        /// Name of the transform that should be rotated around its X axis for wheel rotation<para/>
+        /// May be null if no transform should be rotated
+        /// </summary>
+        [KSPField]
+        public string wheelPivotName;
+
+        /// <summary>
+        /// Name of the mesh that represents the 'non-busted wheel'<para/>
+        /// May be null if no wheel mesh exists or should be manipulated
+        /// </summary>
         [KSPField]
         public string wheelName;
+
+        /// <summary>
+        /// Name of the mesh that represents the 'busted wheel'<para/>
+        /// May be null if no busted wheel mesh exists or should be manipulated (if not present, and wheel is breakable, the normal mesh will be used if it is present)
+        /// </summary>
         [KSPField]
         public string bustedWheelName;
+
+        /// <summary>
+        /// Name of the transform that should be moved for suspension<para/>
+        /// May be null if there is no visual response to suspension forces for the wheel (repulsors)
+        /// </summary>
         [KSPField]
         public string suspensionName;
+
+        /// <summary>
+        /// Name of the transform used for steering rotation.  This transform will be rotated around its Y axis, with the default orientation of the model representing straight forward<para/>
+        /// May be null if there is no steering on the model; may re-use the suspension transform for steering transform if model is setup in such a manner.
+        /// </summary>
         [KSPField]
         public string steeringName;
-        //TODO add toggle control
+
+        /// <summary>
+        /// Determines if this wheel should use tank-steering.  This will adjust the fwd/reverse input for steering input rather than manipulating the orientation of the wheel.
+        /// </summary>
         [KSPField]
+        public bool tankSteering;
+
+        /// <summary>
+        /// If true, steering will be inverted for this wheel.  Toggleable in editor and flight.  Persistent.
+        /// </summary>
+        [KSPField(guiName ="Invert Steering", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_Toggle(enabledText = "Inverted", disabledText = "Normal", suppressEditorShipModified = true)]
         public bool invertSteering;
-        //TODO add toggle control
-        [KSPField]
-        public bool invertMotor;
-        //TODO add toggle control
-        [KSPField]
+
+        /// <summary>
+        /// If true the steering will be locked to zero and will not respond to steering input.
+        /// </summary>
+        [KSPField(guiName = "Steering Lock", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_Toggle(enabledText = "Locked", disabledText = "Free", suppressEditorShipModified = true)]
         public bool steeringLocked;
+
+        /// <summary>
+        /// If true, motor response will be inverted for this wheel.  Toggleable in editor and flight.  Persistent.
+        /// </summary>
+        [KSPField(guiName = "Invert Motor", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_Toggle(enabledText = "Inverted", disabledText = "Normal", suppressEditorShipModified = true)]
+        public bool invertMotor;
+
+        /// <summary>
+        /// If true, motor response will be inverted for this wheel.  Toggleable in editor and flight.  Persistent.
+        /// </summary>
+        [KSPField(guiName = "Motor Lock", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_Toggle(enabledText = "Locked", disabledText = "Enabled", suppressEditorShipModified = true)]
+        public bool motorLocked;
+
         #endregion
 
         #region REGION - Optional wheel parameters that may vary on a part to part basis
@@ -76,6 +132,9 @@ namespace KSPWheel
         #region REGION - Persistent data
         [KSPField(isPersistant = true)]
         public string persistentState = KSPWheelState.DEPLOYED.ToString();
+
+        [KSPField(isPersistant = true)]
+        public bool grounded = false;
         #endregion
 
         #region REGION - Private working/cached variables
@@ -123,7 +182,7 @@ namespace KSPWheel
         
         private void retract()
         {
-            if (wheelState == KSPWheelState.DEPLOYED || wheelState == KSPWheelState.DEPLOYING){ toggleDeploy(); }
+            if (wheelState == KSPWheelState.DEPLOYED || wheelState == KSPWheelState.DEPLOYING) { toggleDeploy(); }
         }
 
         private void toggleDeploy()
@@ -184,7 +243,7 @@ namespace KSPWheel
             if (animationControl != null) { animationControl.setToAnimationState(wheelState, false); }
             Events["toggleGearEvent"].active = animationControl != null;
             Actions["toggleGearAction"].active = animationControl != null;
-
+            Events["repairWheel"].active = wheelState == KSPWheelState.BROKEN;
             Collider[] colliders = part.GetComponentsInChildren<Collider>();
             int len = colliders.Length;
             for (int i = 0; i < len; i++)
@@ -211,6 +270,7 @@ namespace KSPWheel
             wheel.damper = suspensionDamper;
             wheel.motorTorque = motorTorque;
             wheel.brakeTorque = brakeTorque;
+            wheel.grounded = grounded;
         }
 
         /// <summary>
@@ -219,18 +279,26 @@ namespace KSPWheel
         public void FixedUpdate()
         {
             if (!HighLogic.LoadedSceneIsFlight) { return; }
-            if (wheel.rigidBody == null) { wheel.rigidBody = part.GetComponent<Rigidbody>(); }
+            
+            if (wheel.rigidBody == null)
+            {
+                wheel.rigidBody = part.GetComponent<Rigidbody>();
+            }
             if (wheel.rigidBody == null)
             {
                 MonoBehaviour.print("Part rigidbody is null, cannot update!");
                 return;
             }
+            sampleInput();
             //Update the wheel physics state as long as it is not broken or fully retracted
             //yes, this means updates happen during deploy and retract animations (as they should! -- wheels don't just work when they are deployed...).
             if (wheelState != KSPWheelState.BROKEN && wheelState != KSPWheelState.RETRACTED)
             {
                 wheel.UpdateWheel();
             }
+            grounded = wheel.grounded;
+            part.GroundContact = grounded;
+            vessel.checkLanded();
         }
 
         //TODO -- input handling
@@ -240,7 +308,9 @@ namespace KSPWheel
         public void Update()
         {
             if (animationControl != null) { animationControl.updateAnimationState(); }
+            //TODO reset input state on animation state changes, re-orient wheels to default when retracted/ing?
             if (!HighLogic.LoadedSceneIsFlight || wheelState==KSPWheelState.BROKEN || wheelState==KSPWheelState.RETRACTED) { return; }
+            
             //TODO -- input handling/updating
             if (suspensionMesh != null)
             {
@@ -254,7 +324,7 @@ namespace KSPWheel
             }
             if (wheelMesh != null)
             {
-                wheelMesh.transform.position = wheel.wheelMeshPosition;
+                wheelMesh.transform.position = wheel.wheelMeshPosition + wheel.rigidBody.velocity * TimeWarp.fixedDeltaTime;
                 wheelMesh.transform.Rotate(wheel.wheelRPM, 0, 0, Space.Self);
             }
         }
@@ -262,7 +332,30 @@ namespace KSPWheel
         #endregion
 
         #region REGION - Custom update methods
-        
+
+        /// <summary>
+        /// Temporariy input handling code
+        /// </summary>
+        private void sampleInput()
+        {
+            float left = Input.GetKey(KeyCode.A) ? -1 : 0;
+            float right = Input.GetKey(KeyCode.D) ? 1 : 0;
+            float fwd = Input.GetKey(KeyCode.W) ? 1 : 0;
+            float rev = Input.GetKey(KeyCode.S) ? -1 : 0;
+            if (motorLocked) { fwd = rev = 0; }
+            if (steeringLocked) { left = right = 0; }
+            float fwdInput = fwd + rev;
+            float rotInput = left + right;
+            if (invertSteering) { rotInput = -rotInput; }
+            if (tankSteering)
+            {
+                fwdInput = fwdInput + rotInput;
+                if (fwdInput > 1) { fwdInput = 1; }
+                if (fwdInput < -1) { fwdInput = -1; }
+            }
+            wheel.setInputState(fwdInput, rotInput);
+        }
+
         //TODO -- ??
         /// <summary>
         /// Callback from animationControl for when an animation transitions from one state to another
@@ -271,6 +364,16 @@ namespace KSPWheel
         public void onAnimationStateChanged(KSPWheelState state)
         {
             wheelState = state;
+        }
+
+        /// <summary>
+        /// Called from the KSPWheelCollider on first ground contact<para/>
+        /// The input Vector3 is the wheel-local impact velocity.  Relative impact speed can be derived from localImpactVelocity.magnitude
+        /// </summary>
+        /// <param name="localImpactVelocity"></param>
+        public void onWheelImpact(Vector3 localImpactVelocity)
+        {
+
         }
 
         #endregion
