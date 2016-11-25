0d3675448b817c6b383fffe3814c140c99fceb67
shadowmage45 <shadowmage45@hotmail.com>
2016-11-25 11:03:58 -0700

    implement bogey mechanics

--- VSProject/KSPWheel/PartModule/KSPWheelBogey.cs
+++ VSProject/KSPWheel/PartModule/KSPWheelBogey.cs
@@ -10,36 +10,85 @@ namespace KSPWheel
     {
 
         [KSPField]
-        public string bogeyName;
-
-        [KSPField]
-        public Vector3 bogeyUpAxis = Vector3.up;
-
-        [KSPField]
-        public Vector3 bogeyFwdAxis = Vector3.forward;
+        public string bogeyName = "bogey";
 
         [KSPField]
         public Vector3 bogeyRotAxis = Vector3.left;
 
+        [KSPField]
+        public float rotationSpeed = 5f;
+
+        [KSPField]
+        public float restingRotation = 20f;
+
+        [KSPField(guiActive = true)]
+        public float angle;
+
+        private Quaternion defaultRotation;
         private Transform bogeyTransform;
 
         public override void OnStart(StartState state)
         {
             base.OnStart(state);
             bogeyTransform = part.transform.FindRecursive(bogeyName);
+            if (bogeyTransform == null)
+            {
+                MonoBehaviour.print("ERROR: Could not locate bogey for name: " + bogeyName);
+            }
+            defaultRotation = bogeyTransform.localRotation;
         }
 
         internal override void preWheelFrameUpdate()
         {
             base.preWheelFrameUpdate();
+            if (controller.wheelState != KSPWheelState.DEPLOYED)
+            {
+                return;
+            }
             //TODO update bogey orientation
+            if (wheel.isGrounded)
+            {
+                //the 'up' direction of the contacted surface
+                Vector3 normal = wheel.contactNormal;
+                //transformed to local coordinates of the bogey
+                normal = bogeyTransform.InverseTransformDirection(normal);
+                angle = getBogeyAngle(normal);
+                if (rotationSpeed > 0) { angle = Mathf.Lerp(0, angle, Time.deltaTime * rotationSpeed); }
+                bogeyTransform.Rotate(bogeyRotAxis, angle, Space.Self);
+            }
+            else
+            {
+                angle = restingRotation;
+                Quaternion dest = defaultRotation * Quaternion.Euler(angle * bogeyRotAxis);
+                bogeyTransform.localRotation = rotationSpeed > 0 ? Quaternion.Lerp(bogeyTransform.localRotation, dest, Time.deltaTime) : dest;
+            }
+        }
 
-            //the 'up' direction of the contacted surface
-            Vector3 normal = wheel.contactNormal;
-            normal = bogeyTransform.InverseTransformDirection(normal);
-            float dot = Vector3.Dot(normal, bogeyUpAxis);
-            float angle = Mathf.Acos(dot);
-            //now how to know which way to rotate?
+        private float getBogeyAngle(Vector3 localHitNorm)
+        {
+            float a1=0, b1=0;
+            bool invert = false;
+            if (bogeyRotAxis.x != 0)
+            {
+                a1 = localHitNorm.y;
+                b1 = localHitNorm.z;
+                invert = bogeyRotAxis.x < 0;
+            }
+            else if (bogeyRotAxis.y != 0)
+            {
+                a1 = localHitNorm.x;
+                b1 = localHitNorm.z;
+                invert = bogeyRotAxis.y < 0;
+            }
+            else if (bogeyRotAxis.z != 0)
+            {
+                a1 = localHitNorm.x;
+                b1 = localHitNorm.y;
+                invert = bogeyRotAxis.z < 0;
+            }
+            angle = Mathf.Atan2(b1, a1) * Mathf.Rad2Deg;
+            if (invert) { angle = -angle; }
+            return angle;
         }
 
     }
