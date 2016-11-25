499c3ef2502e59de05a6205cc7b09028799d4131
shadowmage45 <shadowmage45@hotmail.com>
2016-11-25 15:29:07 -0700

    add a 'locked suspension' implementation.  Works but could use some cleanup with additional lerp handling

--- VSProject/KSPWheel/PartModule/KSPWheelSuspension.cs
+++ VSProject/KSPWheel/PartModule/KSPWheelSuspension.cs
@@ -24,12 +24,41 @@ namespace KSPWheel
         [KSPField]
         public Vector3 suspensionAxis = Vector3.up;
 
+        [KSPField]
+        public bool allowLockedSuspension = false;
+
+        [KSPField(guiName = "Lock Suspension", guiActive = false, guiActiveEditor = false, isPersistant = true),
+         UI_Toggle(enabledText = "Locked", disabledText = "Free", suppressEditorShipModified = true, affectSymCounterparts = UI_Scene.None)]
+        public bool lockSuspension = false;
+
         private Vector3 defaultPos;
         private Transform suspensionTransform;
+        private GameObject lockedSuspensionObject;
+        private CapsuleCollider lockedSuspensionCollider;
+        private Vector3 lockedPos = Vector3.zero;
+
+        private void suspensionLockChanged(BaseField field, System.Object obj)
+        {
+            if (lockedSuspensionCollider != null)
+            {
+                if (lockSuspension)
+                {
+                    lockedPos = Vector3.zero;
+                    lockedSuspensionCollider.center = lockedPos;
+                    lockedSuspensionCollider.enabled = controller.wheelState == KSPWheelState.DEPLOYED;
+                }
+                else if (!lockSuspension)
+                {
+                    lockedSuspensionCollider.enabled = false;
+                }
+            }
+        }
 
         public override void OnStart(StartState state)
         {
             base.OnStart(state);
+            Fields[nameof(lockSuspension)].guiActive = allowLockedSuspension;
+            Fields[nameof(lockSuspension)].uiControlFlight.onFieldChanged = suspensionLockChanged;
         }
 
         internal override void postControllerSetup()
@@ -56,7 +85,37 @@ namespace KSPWheel
                 suspensionTransform.localPosition = defaultPos;
                 suspensionTransform.position -= o * offset;
             }
+            if (lockedSuspensionCollider != null)
+            {
+                lockedSuspensionCollider.enabled = controller.wheelState == KSPWheelState.DEPLOYED && lockSuspension;
+            }
         }
 
+        internal override void preWheelPhysicsUpdate()
+        {
+            base.preWheelPhysicsUpdate();
+            if (lockSuspension)
+            {
+                if (lockedSuspensionObject == null)
+                {
+                    lockedSuspensionObject = new GameObject("KSPWheelLockedSuspension");
+                    lockedSuspensionObject.transform.NestToParent(wheelTransform);
+                    lockedSuspensionObject.layer = 26;
+                    lockedSuspensionCollider = lockedSuspensionObject.AddComponent<CapsuleCollider>();
+                    lockedSuspensionCollider.radius = wheel.radius;
+                    lockedSuspensionCollider.height = wheel.radius * 2;
+                    lockedPos.y = -(wheel.length - wheel.compressionDistance);
+                    lockedSuspensionCollider.center = lockedPos;
+                }
+                float extSpeed = 0.05f*Time.fixedDeltaTime;
+                float d = Mathf.Abs(lockedPos.y) - wheel.length * 0.95f;
+                if (d < -extSpeed) { d = -extSpeed; }
+                if (d != 0)
+                {
+                    lockedPos.y += d;
+                    lockedSuspensionCollider.center = lockedPos;
+                }
+            }
+        }
     }
 }
