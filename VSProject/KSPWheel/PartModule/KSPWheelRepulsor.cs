c279dd35bf3853a576508de2b3f306ce18e5a494
shadowmage45 <shadowmage45@hotmail.com>
2016-11-27 17:02:12 -0700

    add very basic repulsor module

---
+++ VSProject/KSPWheel/PartModule/KSPWheelRepulsor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel
+{
+    public class KSPWheelRepulsor : KSPWheelSubmodule
+    {
+
+        [KSPField(guiName = "Repulsor Height", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = 0, maxValue = 5, stepIncrement = 0.1f, suppressEditorShipModified = true)]
+        public float repulsorHeight = 1f;
+
+        internal override void preWheelPhysicsUpdate()
+        {
+            base.preWheelPhysicsUpdate();
+            wheel.length = repulsorHeight;
+            //TODO add a horizontal force based on surface normal vs. suspension normal
+
+        }
+
+    }
+}
