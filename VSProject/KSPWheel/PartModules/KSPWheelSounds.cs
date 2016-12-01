756c80f61fa8a9a1d3fa5c113b6eb877fb1bdcd0
shadowmage45 <shadowmage45@hotmail.com>
2016-11-30 18:51:49 -0700

    rename source directory to prevent conflict with PartModule class vs KSPWheel.PartModule namespace

---
+++ VSProject/KSPWheel/PartModules/KSPWheelSounds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel
+{
+    public class KSPWheelSounds : PartModule
+    {
+
+        [KSPField]
+        public string longSlipEffect = String.Empty;
+
+        [KSPField]
+        public float longSlipStart = 0.2f;
+
+        [KSPField]
+        public float longSlipPeak = 0.6f;
+
+        [KSPField]
+        public string latSlipEffect = String.Empty;
+
+    }
+}
