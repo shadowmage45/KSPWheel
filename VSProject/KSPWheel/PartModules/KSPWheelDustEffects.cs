3a4757d267edab441d39b2025a0c582cb7703ddb
shadowmage45 <shadowmage45@hotmail.com>
2017-01-16 15:39:42 -0700

    initial layout for dust-fx work -- going with full custom implementation, using original KF-dustfx as reference only for some of the particle stuff I'm unfamiliar with

---
+++ VSProject/KSPWheel/PartModules/KSPWheelDustEffects.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel
+{
+    public class KSPWheelDustEffects : KSPWheelSubmodule
+    {
+        /// <summary>
+        /// Below this value of spring force / spring loading, no contribution to emission will come from spring force
+        /// </summary>
+        [KSPField]
+        public float minDustForce = 0.1f;
+
+        /// <summary>
+        /// Below this value of linear speed no contribution to emission will come from wheel speed/rpm
+        /// </summary>
+        [KSPField]
+        public float minDustSpeed = 0.1f;
+
+        /// <summary>
+        /// Multiplier to dust emission contribution from spring force
+        /// </summary>
+        [KSPField]
+        public float dustForceMult = 1f;
+
+        /// <summary>
+        /// Multiplier to dust emission contribution from wheel velocity/rpm
+        /// </summary>
+        [KSPField]
+        public float dustSpeedMult = 1f;
+
+        /// <summary>
+        /// The name of the particle effect to use for dust creation
+        /// </summary>
+        [KSPField]
+        public string dustParticleEffect = "Effects/fx_smokeTrail_light";
+
+        private bool setupEmitters = false;
+        private GameObject[] dustEmitters;//one emitter per wheel collider on the part; so that tracks/compound parts still throw up dust on a per-wheel basis
+        private ParticleAnimator[] animators;//no clue if these will even work in modern Unity versions, will investigate ParticleSystem instead...
+
+        internal override void preWheelFrameUpdate()
+        {
+            base.preWheelFrameUpdate();
+            if (!HighLogic.LoadedSceneIsFlight || !FlightGlobals.ready) { return; }
+            if (controller.wheelState != KSPWheelState.DEPLOYED) { return; }
+            if (!setupEmitters) { setupDustEmitters(); }
+        }
+
+        private void setupDustEmitters()
+        {
+            setupEmitters = true;
+        }
+    }
+
+    //TODO -- this needs to be a KSPAddon with a game-database-loaded callback and MM database reloaded callbacks
+    public static class DustColors
+    {
+        private static Dictionary<String, BodyDustColors> bodyDustColors = new Dictionary<string, BodyDustColors>();
+        public static readonly Color defaultColor = new Color(0.75f, 0.75f, 0.75f, 0.014f);
+
+        public static Color getBodyColor(string body, string biome)
+        {
+            if (bodyDustColors.ContainsKey(body))
+            {
+                return bodyDustColors[body].getBiomeColor(biome);
+            }
+            return defaultColor;
+        }
+
+        public static void loadDustColors()
+        {
+            //TODO -- when would loading of the config values be appropriate outside of a module-manager context?
+            //TODO -- can only really load them on game-database finished processing
+            //TODO -- add a MM callback for -re-loading- of the color values
+            bodyDustColors.Clear();
+            ConfigNode[] potentialColorNodes = GameDatabase.Instance.GetConfigNodes("DustColorDefinitions");
+            if (potentialColorNodes == null || potentialColorNodes.Length <= 0) { return; }
+            ConfigNode bodyDustColorNode = potentialColorNodes[0];
+            ConfigNode bodyNode;
+            ConfigNode biomeNode;
+            String bodyName, biomeName;
+            Color color;
+            int len = bodyDustColorNode.nodes.Count;
+            for (int i = 0; i < len; i++)
+            {
+                bodyNode = bodyDustColorNode.nodes[i];
+                bodyName = bodyNode.name;
+                int len2 = bodyNode.nodes.Count;
+                for (int k = 0; k < len2; k++)
+                {
+                    biomeNode = bodyNode.nodes[k];
+                    biomeName = biomeNode.name;
+                    color = ConfigNode.ParseColor(biomeNode.GetStringValue("Color", "0.75, 0.75, 0.75, 0.014"));
+                }
+            }
+        }
+
+        private static void loadColor(string body, string biome, Color color)
+        {
+            if (!bodyDustColors.ContainsKey(body))
+            {
+                bodyDustColors.Add(body, new BodyDustColors());
+            }
+            bodyDustColors[body].addColor(biome, color);
+        }
+    }
+
+    public class BodyDustColors
+    {
+        private Dictionary<String, Color> biomeDustColors = new Dictionary<string, Color>();
+        private Color bodyDefaultColor;
+
+        public Color getBiomeColor(string name)
+        {
+            if (biomeDustColors.ContainsKey(name)) { return biomeDustColors[name]; }
+            return bodyDefaultColor == null? DustColors.defaultColor : bodyDefaultColor;
+        }
+
+        public void addColor(string biomeName, Color color)
+        {
+            if (biomeName.ToLower().Equals("default"))
+            {
+                bodyDefaultColor = color;
+                return;
+            }
+            if (!biomeDustColors.ContainsKey(biomeName))
+            {
+                biomeDustColors.Add(biomeName, color);
+            }
+        }
+    }
+}
