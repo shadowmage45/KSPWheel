78c9c174546ef7eb4e03661c78c1d8a48987dcff
shadowmage45 <shadowmage45@hotmail.com>
2017-01-19 22:55:23 -0700

    link repulsor to dust module water code prep work for repulsor lights -- may not use them as they don't really add much prep work for repulsor gimbaling test prep work for repulsor texture animation

--- VSProject/KSPWheel/PartModules/KSPWheelRepulsor.cs
+++ VSProject/KSPWheel/PartModules/KSPWheelRepulsor.cs
@@ -24,9 +24,9 @@ namespace KSPWheel
          UI_Toggle(enabledText = "Offset", disabledText = "Standard", suppressEditorShipModified = true)]
         public bool forcePointOffset = true;
 
-        [KSPField(guiName = "Force Axis", guiActiveEditor = false, guiActive = true),
+        [KSPField(guiName = "Force Axis", guiActiveEditor = false, guiActive = false),
          UI_Toggle(enabledText = "Suspension", disabledText = "HitNormal", suppressEditorShipModified = true)]
-        public bool suspensionNormal = true;
+        public bool suspensionNormal = false;
 
         [KSPField]
         public float easeTimeMult = 0.5f;
@@ -37,9 +37,44 @@ namespace KSPWheel
         [KSPField]
         public float energyUse = 1f;
 
-        private float currentRepulsorLength;
-        private float desiredRepulsorLength;
+        [KSPField]
+        public float animSpeed = 1f;
 
+        [KSPField]
+        public int animAxis = 0;
+
+        [KSPField]
+        public bool gimbaled = false;
+
+        [KSPField]
+        public string gimbalName = String.Empty;
+
+        [KSPField]
+        public string gridName = String.Empty;
+
+        [KSPField(guiName = "Light L", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = 0.1f, maxValue = 1, stepIncrement = 0.01f, suppressEditorShipModified = true)]
+        public float lightLength = 10f;
+
+        [KSPField(guiName = "Light A", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = 0.1f, maxValue = 1, stepIncrement = 0.01f, suppressEditorShipModified = true)]
+        public float lightAngle = 30f;
+
+        [KSPField(guiName = "Light I", guiActive = true, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = 0.1f, maxValue = 1, stepIncrement = 0.01f, suppressEditorShipModified = true)]
+        public float lightIntensity = 1f;
+
+        private KSPWheelDustEffects dustModule;
+
+        private Transform gimbalTransform;
+
+        private GameObject lightObject;
+        private Light repulsorLight;
+
+        private Material gridMaterial;
+
+        private float curLen;
+        private float destLen;
         private Vector3 oceanHitPos;
 
         private void repulsorToggled(BaseField field, System.Object obj)
@@ -64,33 +99,73 @@ namespace KSPWheel
             this.wheelGroupUpdate(int.Parse(controller.wheelGroup), m =>
             {
                 m.repulsorHeight = repulsorHeight;
-                desiredRepulsorLength = m.repulsorHeight;
+                destLen = m.repulsorHeight;
             });
         }
 
         public override void OnStart(StartState state)
         {
             base.OnStart(state);
-            oceanHitPos = part.transform.position;
+            if (!string.IsNullOrEmpty(gimbalName)) { gimbalTransform = part.transform.FindRecursive(gimbalName); }
             Fields[nameof(repulsorEnabled)].uiControlFlight.onFieldChanged = repulsorToggled;
             Fields[nameof(repulsorHeight)].uiControlFlight.onFieldChanged = Fields[nameof(repulsorHeight)].uiControlEditor.onFieldChanged = repulsorHeightUpdated;
-            currentRepulsorLength = desiredRepulsorLength = repulsorHeight;
+            curLen = destLen = repulsorHeight;
         }
 
         internal override void postWheelCreated()
         {
             base.postWheelCreated();
+            dustModule = part.GetComponent<KSPWheelDustEffects>();
+            if (lightObject == null)
+            {
+                lightObject = new GameObject("RepulsorLight");
+                lightObject.transform.parent = wheel.transform;
+                lightObject.transform.position = wheel.transform.position;
+                repulsorLight = lightObject.AddComponent<Light>();
+                repulsorLight.type = LightType.Point;//do we really want a point light?  wouldn't spot make more sense? (maybe not with the gimballed repulsor) -- perhaps configurable?
+                repulsorLight.renderMode = LightRenderMode.ForceVertex;//low quality
+                repulsorLight.shadows = LightShadows.None;
+                repulsorLight.range = 0f;
+                repulsorLight.color = Color.clear;
+                repulsorLight.intensity = 0f;
+                repulsorLight.enabled = false;
+            }
+        }
+
+        internal override void preWheelFrameUpdate()
+        {
+            base.preWheelFrameUpdate();
+            bool lightActive = repulsorLight.enabled;
+            if (!repulsorEnabled)
+            {
+                if (lightActive)
+                {
+                    repulsorLight.enabled = false;
+                }
+            }
+            else
+            {
+                if (!lightActive)
+                {
+                    repulsorLight.enabled = true;
+                    MonoBehaviour.print("activating light");
+                }
+                repulsorLight.color = Color.blue;
+                repulsorLight.range = 20f;
+                repulsorLight.intensity = 5f;// Mathf.Clamp(wheel.springForce * 0.01f, 0.4f, 0.8f);
+            }
         }
 
         internal override void preWheelSuspensionCalc()
         {
             base.preWheelSuspensionCalc();
             //update repulsor 'length' stats
-            wheel.length = currentRepulsorLength * 5f;
-            currentRepulsorLength = Mathf.MoveTowards(currentRepulsorLength, desiredRepulsorLength, 1 * Time.fixedDeltaTime);
+            wheel.length = curLen * 5f;
+            curLen = Mathf.MoveTowards(curLen, destLen, 1 * Time.fixedDeltaTime);
             wheel.useSuspensionNormal = suspensionNormal;
             wheel.forceApplicationOffset = forcePointOffset ? 1f : 0f;
             wheel.useExternalHit = false;
+            if (dustModule != null) { dustModule.waterMode = false; }
             if (vessel.mainBody.ocean)
             {
                 float alt = FlightGlobals.getAltitudeAtPos(wheel.transform.position);
@@ -101,6 +176,7 @@ namespace KSPWheel
                 }
                 Vector3 surfaceNormal = vessel.mainBody.GetSurfaceNVector(vessel.latitude, vessel.longitude);
                 Vector3 pointOnSurface = wheel.transform.position - alt * surfaceNormal;
+                Vector3 oceanHitPos = Vector3.zero;
                 if (rayPlaneIntersect(wheel.transform.position, -wheel.transform.up, pointOnSurface, surfaceNormal, out oceanHitPos))
                 {
                     RaycastHit hit;
@@ -122,6 +198,7 @@ namespace KSPWheel
                         wheel.useExternalHit = true;
                         wheel.externalHitPoint = oceanHitPos;
                         wheel.externalHitNormal = surfaceNormal;
+                        if (dustModule != null) { dustModule.waterMode = true; }
                     }
                 }
             }
