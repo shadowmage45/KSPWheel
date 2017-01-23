88d7fd6d6a9a1feee4c4db39f94eb521e95b7b8e
shadowmage45 <shadowmage45@hotmail.com>
2017-01-23 10:58:07 -0700

    deactivate dust-fx when game is paused (is there a cleaner method than the events?  Is there not a 'isPaused' state flag somewhere I can poll/query?

--- VSProject/KSPWheel/PartModules/KSPWheelDustEffects.cs
+++ VSProject/KSPWheel/PartModules/KSPWheelDustEffects.cs
@@ -65,6 +65,8 @@ namespace KSPWheel
 
         public bool waterMode = false;
 
+        private bool gamePaused = false;
+
         private Color[] colArr = new Color[5];
         private string prevBody = string.Empty;
         private string prevBiome = string.Empty;
@@ -80,6 +82,8 @@ namespace KSPWheel
         public override void OnStart(StartState state)
         {
             base.OnStart(state);
+            GameEvents.onGamePause.Add(new EventVoid.OnEvent(onGamePause));
+            GameEvents.onGameUnpause.Add(new EventVoid.OnEvent(onGameUnpause));
         }
 
         public override void OnDestroy()
@@ -95,6 +99,18 @@ namespace KSPWheel
                     GameObject.Destroy(dustObjects[i]);
                 }
             }
+            GameEvents.onGamePause.Remove(new EventVoid.OnEvent(onGamePause));
+            GameEvents.onGameUnpause.Remove(new EventVoid.OnEvent(onGameUnpause));
+        }
+
+        private void onGamePause()
+        {
+            gamePaused = true;
+        }
+
+        private void onGameUnpause()
+        {
+            gamePaused = false;
         }
 
         internal override void preWheelFrameUpdate()
@@ -136,6 +152,7 @@ namespace KSPWheel
 
         private void updateDustEmission()
         {
+            if (gamePaused) { return; }//game paused...
             int len = dustObjects.Length;
             if (HighLogic.CurrentGame.Parameters.CustomParams<KSPWheelSettings>().wheelDustCamera)
             {
