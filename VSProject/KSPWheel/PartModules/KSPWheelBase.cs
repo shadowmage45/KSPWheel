37f82dd56fba87144844d199a416e3a2913d5f26
shadowmage45 <shadowmage45@hotmail.com>
2017-01-24 13:56:37 -0700

    experiments towards a new suspension auto-tuning method

--- VSProject/KSPWheel/PartModules/KSPWheelBase.cs
+++ VSProject/KSPWheel/PartModules/KSPWheelBase.cs
@@ -589,7 +589,6 @@ namespace KSPWheel
             float spring, damper, springLoad, natFreq, criticalDamping;
             float compression = 0;
             float lengthCorrectedMass;
-            float compressionRate;
             int len = wheelData.Length;
             KSPWheelData data;
             for (int i = 0; i < len; i++)
@@ -597,8 +596,6 @@ namespace KSPWheel
                 data = wheelData[i];
                 compression = data.wheel.compressionDistance / data.wheel.length;
                 lengthCorrectedMass = vesselMass / data.wheel.length;
-                //TODO
-                compressionRate = 0f;
                 if (wheelRepairTimer < 1)
                 {
                     data.timeBoostFactor = 0f;
@@ -620,8 +617,8 @@ namespace KSPWheel
                     }
                     data.timeBoostFactor = Mathf.Clamp(data.timeBoostFactor, 0.01f, 0.85f);
                     compressionBoostFactor = 1.0f + Mathf.Clamp(compression * 2f, 0, 1f);
-                    //spring = Mathf.Clamp(lengthCorrectedMass * evaluateCurve(compressionBoostFactor, data.timeBoostFactor) * springRating * 10f, 0.01f, 50000f);
-                    spring = calculateSpringFactor(compression, data.wheel.spring);
+                    spring = Mathf.Clamp(lengthCorrectedMass * evaluateCurve(compressionBoostFactor, data.timeBoostFactor) * springRating * 10f, 0.01f, 50000f);
+                    spring = lengthCorrectedMass * calculateSpring(compression, data.timeBoostFactor, 0.2f, 0.8f);
                     springLoad = spring * data.wheel.length * 0.5f * 0.1f;//target load for damper calc is spring at half compression
                     natFreq = Mathf.Sqrt(spring / springLoad);//natural frequency
                     criticalDamping = 2 * springLoad * natFreq;//critical damping
@@ -636,27 +633,37 @@ namespace KSPWheel
             }
         }
 
+        /// <summary>
+        /// Comp = 0-1 compression
+        /// Time = -1 - 1 time factor, based on response to compression (-1 means undercompressed for duration, 1 means overcompressed for duration, 0 means no duration)
+        /// Lower = lower stability bounds, below this compression it is considered 'undercompressed' and spring value is lowered
+        /// Upper = upper stability bounds, above this compression it is considered 'overcompressed' and spring value is raised
+        /// </summary>
+        /// <returns></returns>
+        private float calculateSpring(float comp, float time, float lower, float upper)
+        {
+            float n1 = 1 / lower;
+            float n2 = 1 / (1-upper);
+            float compFactor = 0;
+            if (comp < lower)
+            {
+                compFactor = (comp - lower) * n1;
+            }
+            else if(comp > upper)
+            {
+                compFactor = (comp - upper) * n2;
+            }
+            compFactor += 1;
+            float timeFactor = 1 + Mathf.Abs(time * time * time);
+            return Mathf.Pow(compFactor, timeFactor);
+        }
+
         //TODO derive cleaner curves / alternate curves / alternate spring-setting methods for over and under compression
         // can use a curve that starts at y=1 when x=comp limit
         private float evaluateCurve(float comp, float time)
         {
             return Mathf.Clamp(1f / Mathf.Abs(1f - 2f / Mathf.Pow(comp, time)), 0.01f, 10000000f);
         }
-
-        /// <summary>
-        /// Calculate the current auto-adjustment to spring ratio based on the current compression, duration of over/under-compression, and rate of compression change
-        /// </summary>
-        /// <param name="compression"></param>
-        /// <param name="duration"></param>
-        /// <returns></returns>
-        private float calculateSpringFactor(float compression, float currentSpring)
-        {
-            pid.setParams(kT, kP, kI, kD);
-            float output = pid.update(compression, Time.fixedDeltaTime);
-            return currentSpring * output;
-        }
-
-        private Utils.PIDController pid = new Utils.PIDController(0.5f, 0.5f, 1, 0, 0);
 
         internal void addSubmodule(KSPWheelSubmodule module)
         {
