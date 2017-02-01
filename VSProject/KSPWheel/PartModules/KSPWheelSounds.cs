26711dcd89d9145f8f39a974d6ffcae714f0ecdb
shadowmage45 <shadowmage45@hotmail.com>
2017-01-31 20:20:10 -0700

    updates to sounds module to clean up specifications and calculations of power inputs

--- VSProject/KSPWheel/PartModules/KSPWheelSounds.cs
+++ VSProject/KSPWheel/PartModules/KSPWheelSounds.cs
@@ -22,19 +22,22 @@ namespace KSPWheel
         public string latSlipEffect = String.Empty;
 
         [KSPField]
-        public float latSlipStart = 0.2f;
+        public float latSlipStart = 0.15f;
 
         [KSPField]
-        public float latSlipPeak = 0.6f;
+        public float latSlipPeak = 0.5f;
+
+        [KSPField]
+        public float latSlipStartVelocity = 0.075f;
+
+        [KSPField]
+        public float latSlipPeakVelocity = 2f;
 
         [KSPField]
         public string motorEffect = String.Empty;
 
         [KSPField]
         public string runningEffect = String.Empty;
-
-        [KSPField]
-        public float runningRpmPeak = 200f;
 
         private KSPWheelMotor motor;
 
@@ -47,18 +50,18 @@ namespace KSPWheel
         internal override void preWheelFrameUpdate()
         {
             base.preWheelFrameUpdate();
+
             if (!string.IsNullOrEmpty(runningEffect))
             {
-                float rpm = Mathf.Abs(wheel.rpm);
-                float power = rpm > runningRpmPeak ? 1 : rpm / runningRpmPeak;
+                float velocity = Mathf.Abs(wheel.wheelLocalVelocity.z);
+                float max = controller.maxSpeed * controller.wheelMaxSpeedScalingFactor;
+                float power = velocity > max ? 1 : velocity / max;
                 part.Effect(runningEffect, power);
             }
 
             if (!string.IsNullOrEmpty(motorEffect) && motor != null)
             {
-                float rpm = Mathf.Abs(wheel.rpm);
-                rpm *= motor.gearRatio;
-                float power = rpm > motor.maxRPM ? 1 : rpm / motor.maxRPM;
+                float power = Mathf.Abs(wheel.motorTorque) / motor.gearRatio / motor.maxMotorTorque;
                 part.Effect(motorEffect, power);
             }
 
@@ -72,8 +75,19 @@ namespace KSPWheel
 
             if (!string.IsNullOrEmpty(latSlipEffect))
             {
-                float div = 1 / (latSlipPeak - latSlipStart);
-                float power = Mathf.Max(Mathf.Min((wheel.lateralSlip - latSlipStart) * div, 1), 0);
+                float power = 0;
+                float velocity = Mathf.Abs(wheel.wheelLocalVelocity.x);
+                float slip = wheel.lateralSlip;
+                if (velocity > latSlipStartVelocity && slip > latSlipStart)
+                {
+                    float div = 1 / (latSlipPeak - latSlipStart);//range
+                    float slipFactor = (slip - latSlipStart) * div;
+                    slipFactor = Mathf.Clamp01(slipFactor);
+                    div = 1 / (latSlipPeakVelocity - latSlipStartVelocity);
+                    float velocityFactor = (velocity - latSlipStartVelocity) * div;
+                    velocityFactor = Mathf.Clamp01(velocityFactor);
+                    power = slipFactor * velocityFactor;
+                }
                 part.Effect(latSlipEffect, power);
             }
         }
