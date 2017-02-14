bde0c825427a90474f88d057c8454a2acb3a7be7
shadowmage45 <shadowmage45@hotmail.com>
2017-02-14 15:39:23 -0700

    initial layout work for new constraints module and updated ALG module * constraints module needs major work; rotation and position should be working, but look-at is...not right * ALG module only has bare framework and some notes, needs major work to finish, along with the re-rigging of all of the models.

---
+++ VSProject/KSPWheel/PartModules/KSPWheelAdjustableGear2.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel
+{
+    public class KSPWheelAdjustableGear2 : KSPWheelSubmodule
+    {
+
+        [KSPField]
+        public string suspensionUpperContainerName = string.Empty;
+
+        [KSPField]
+        public string suspensionLowerContainerName = string.Empty;
+
+        [KSPField]
+        public string wheelContainerName = string.Empty;
+
+        [KSPField]
+        public string rearDoorName = string.Empty;
+
+        [KSPField]
+        public string rightDoorName = string.Empty;
+
+        [KSPField]
+        public string leftDoorName = string.Empty;
+
+        [KSPField]
+        public string mainStrutName = string.Empty;
+
+        [KSPField]
+        public string wheelHousingName = string.Empty;
+
+        [KSPField]
+        public string wheelBogeyName = string.Empty;
+
+        [KSPField]
+        public string wheelMeshName = string.Empty;
+
+        [KSPField]
+        public bool canFlip = false;
+
+        [KSPField]
+        public float minExtension = 0.25f;
+
+        [KSPField]
+        public float maxExtension = 0.5f;
+
+        [KSPField]
+        public float minSuspensionLength = 0.25f;
+
+        [KSPField]
+        public float maxSuspensionLength = 0.25f;
+
+        [KSPField]
+        public float minAngle = 0f;
+
+        [KSPField]
+        public float maxAngle = 30f;
+
+        [KSPField]
+        public float mainStrutRetractedAngle = 0f;
+
+        [KSPField]
+        public float wheelHousingRetractedAngle = 0f;
+
+        [KSPField]
+        public float wheelBogeyRetractedAngle = 0f;
+
+        /// <summary>
+        /// User-configured main strut angle in editor
+        /// </summary>
+        [KSPField(guiName = "Strut Angle", guiActive = false, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = -30f, maxValue = 30f, stepIncrement = 0.1f, suppressEditorShipModified = true)]
+        public float currentPrimaryAngle = 0f;
+
+        /// <summary>
+        /// Calculated secondary angle for wheel container to keep aligned with ground
+        /// </summary>
+        [KSPField(guiName = "Wheel Angle", guiActive = false, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = -30f, maxValue = 30f, stepIncrement = 0.1f, suppressEditorShipModified = true)]
+        public float currentSecondaryAngle = 0f;
+
+        /// <summary>
+        /// Has this part been marked as a clone?  Set during part symmetry setups.  Determines initial 'flipped' state if wheel can be flipped.
+        /// Also determines the direction of movement for the user-selectable 'angle' controls.
+        /// </summary>
+        [KSPField(isPersistant = true)]
+        public bool isClone = false;
+
+        /// <summary>
+        /// Has user selected flipped wheel or set automatically from cloned state.  Determines wheel angle offset direction, wheel spin direction, others...
+        /// </summary>
+        [KSPField(isPersistant = true)]
+        public bool isFlipped = false;
+
+        private Transform suspensionUpperContainer;
+        private Transform suspensionLowerContainer;
+        private Transform wheelContainer;//used to position wheel collider according to model setup -AND- to rotate wheel for 'flip'
+        private Transform mainStrut;
+        private Transform wheelHousing;
+        private Transform wheelMesh;
+
+        /// <summary>
+        /// Serialize these fields across parts (prefab -> editor; editor -> cloned)
+        /// </summary>
+        [SerializeField]
+        private bool initializedDefaultRotations;
+        [SerializeField]
+        private Quaternion mainStrutDefaultRotation;
+        [SerializeField]
+        private Quaternion wheelHousingDefaultRotation;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            locateTransforms();
+        }
+
+        internal override void postWheelCreated()
+        {
+            base.postWheelCreated();
+        }
+
+        private void locateTransforms()
+        {
+            suspensionUpperContainer = part.transform.FindRecursive(suspensionUpperContainerName);
+            suspensionLowerContainer = part.transform.FindRecursive(suspensionLowerContainerName);
+            wheelContainer = part.transform.FindRecursive(wheelContainerName);
+            mainStrut = part.transform.FindRecursive(mainStrutName);
+            wheelHousing = part.transform.FindRecursive(wheelHousingName);
+            wheelMesh = part.transform.FindRecursive(wheelMeshName);
+            if (!initializedDefaultRotations)
+            {
+                initializedDefaultRotations = true;
+                mainStrutDefaultRotation = mainStrut.localRotation;
+                wheelHousingDefaultRotation = wheelHousing.localRotation;
+            }
+        }
+
+        private void positionTransforms()
+        {
+
+        }
+
+        private void updateAnimation()
+        {
+
+        }
+
+        /**
+         * Basic Overview of ALG rigging.
+         * Important Transforms:
+         * Suspension Upper Container
+         *   Used to effect main strut angle setup
+         * Wheel Container
+         *   Contains wheel collider and suspension lower container
+         *   Used to position wheel collider into its 'compressed' position.  Drives constraint handling for most of the rest of the transforms.
+         * Suspension Lower Container
+         *   Used by suspension module to position transforms according to wheel compression
+         * Main Strut Upper
+         *   Used to drive retract animation.  Most of the rest of the animation is handled through constraints
+         *   
+         * --------------------------------------------------------------------
+         * Animation handling:
+         * (Retract)
+         * 1. Lerp main strut towards straight-downward extended position
+         *    Lerp wheel alignment into straight-upwards extended position
+         * 2. Open the 'main doors' to allow for main strut to retract
+         * 3. Retract wheel extension from extended to retracted position
+         * 4. Rotate main wheel strut from extended to retracted position
+         *    Rotate wheel housing from extended to retracted position
+         * 5. Close all doors
+         * 
+         * (Deploy)
+         * 1.) Open all doors
+         * 2.) Rotate main wheel strut from retracted to extended position
+         *     Rotate wheel housing from retracted to extended position
+         * 3.) Close main doors
+         * 4.) Extend strut extension from retracted to user-set position
+         * 5.) Rotate wheel from extended to user-set configuration.
+         * 
+         * --------------------------------------------------------------------
+         * Full Time External Module Constraints:
+         * DragBraceUpper -> DragBraceLower (locked look)
+         * DragBraceLower -> DragBraceUpper (locked look)
+         * WheelContainer -> WheelTarget (position)
+         * SuspensionContainerLower -> SuspensionContainerUpper (locked look)
+         * SuspensionContainerUpper -> SuspensionContainerLower (locked look)
+         * 
+         * --------------------------------------------------------------------
+         * ALG Module Managed Constraints:
+         * WheelContainer -> UserSetRotation (rotation)
+         * SuspensionContainerLower -> compression from collider (position)
+         * WheelMesh -> rpm-based rotation from collider (rotation)
+         * 
+         **/
+    }
+}
---
+++ VSProject/KSPWheel/PartModules/KSPWheelConstraints.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace KSPWheel.PartModules
+{
+    public class KSPWheelConstraints : PartModule
+    {
+        [Persistent]
+        public string configNodeData = string.Empty;
+
+        private bool initialized = false;
+        private ConstraintData[] constraints;
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            initializeConstraints();
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            initializeConstraints();
+        }
+
+        public void reloadConstraints()
+        {
+            initialized = false;
+            constraints = null;
+            initializeConstraints();
+        }
+
+        public void initializeConstraints()
+        {
+            if (initialized) { return;}
+            initialized = true;
+            ConfigNode root = ConfigNode.Parse(configNodeData).nodes[0];
+            ConfigNode[] constraintNodes = root.GetNodes("CONSTRAINT");            
+            int len = constraintNodes.Length;
+            constraints = new ConstraintData[len];
+            for (int i = 0; i < len; i++)
+            {
+                constraints[i] = new ConstraintData(part, constraintNodes[i]);
+            }
+            //force contraints to update once after initialized
+            updateConstraints();
+        }
+
+        public void Update()
+        {
+            updateConstraints();
+        }
+
+        private void updateConstraints()
+        {
+            int len = constraints.Length;
+            for (int i = 0; i < len; i++)
+            {
+                constraints[i].updateConstraint();
+            }
+        }
+
+    }
+
+    public class ConstraintData
+    {
+        private Transform mover;
+        private Transform target;
+        private ConstraintType type = ConstraintType.POSITION;
+        private Vector3 mainAxis;
+        private Vector3 secAxis;
+
+        public ConstraintData(Part part, ConfigNode node)
+        {
+            string moverDef = node.GetStringValue("mover");
+            string[] moverSplit = moverDef.Split(',');
+            string moverName = moverSplit[0];
+            int moverIndex = moverSplit.Length >= 2 ? int.Parse(moverSplit[1]) : 0;
+            mover = part.transform.FindChildren(moverName)[moverIndex];
+
+            string targetDef = node.GetStringValue("target");
+            string[] targetSplit = targetDef.Split(',');
+            string targetName = targetSplit[0];
+            int targetIndex = targetSplit.Length >= 2 ? int.Parse(targetSplit[1]) : 0;
+            target = part.transform.FindChildren(targetName)[targetIndex];
+
+            if (target == null || mover == null)
+            {
+                MonoBehaviour.print("ERROR SETTING UP CONSTRAINT.  Either mover or target was null for definitions: " + moverDef + " :: " + targetDef+".\n"+
+                    "Please check your definitions and correct the error in the model or config.");
+            }
+            try
+            {
+                type = (ConstraintType)Enum.Parse(typeof(ConstraintType), node.GetStringValue("type", "POSITION"), true);
+            }
+            catch (Exception e)
+            {
+                MonoBehaviour.print("ERROR PARSING CONSTRAINT TYPE: " + node.GetStringValue("type") + " is not a recongnized constraint type.  Setting to default of: POSITION");
+                MonoBehaviour.print("Valid types are: POSITION, ROTATION, LOOKFREE, LOOKLOCK");
+                MonoBehaviour.print(e);
+                type = ConstraintType.POSITION;
+            }
+        }
+
+        public void updateConstraint()
+        {
+            switch (type)
+            {
+                case ConstraintType.POSITION:
+                    updatePosition();
+                    break;
+                case ConstraintType.ROTATION:
+                    updateRotation();
+                    break;
+                case ConstraintType.LOOKFREE:
+                    updateLookFree();
+                    break;
+                case ConstraintType.LOOKLOCK:
+                    updateLookLocked();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void updatePosition()
+        {
+            mover.position = target.position;
+        }
+
+        private void updateRotation()
+        {
+            mover.localRotation = target.rotation;
+        }
+
+        private void updateLookFree()
+        {
+            //TODO -- adapt to use specified axis
+            mover.LookAt(target, mover.up);
+        }
+
+        private void updateLookLocked()
+        {
+            //TODO -- may need to use the position difference and convert via direction to avoid scaling problems
+            //TODO -- uhh..yah..something in here needs adjusted for the 'main axis'.
+            Vector3 targetPos = target.position;
+            Vector3 localDiff = mover.InverseTransformPoint(targetPos);//position of the target, in local space (origin=0,0,0)
+            Vector3 localRotation = Vector3.zero;
+            float rx = 0f, ry = 0f, rz = 0f;
+            if (secAxis.x != 0)
+            {
+                //use y and z
+                rx = Mathf.Atan2(localDiff.y, localDiff.z) * Mathf.Rad2Deg;
+            }
+            else if (secAxis.y != 0)
+            {
+                //use x and z
+                ry = Mathf.Atan2(localDiff.x, localDiff.z) * Mathf.Rad2Deg;
+            }
+            else if (secAxis.z != 0)
+            {
+                //use x and y
+                rz = Mathf.Atan2(localDiff.x, localDiff.y) * Mathf.Rad2Deg;
+            }
+            mover.Rotate(rx, ry, rz, Space.Self);
+        }
+
+    }
+
+    public enum ConstraintType
+    {
+        POSITION,
+        ROTATION,
+        LOOKFREE,
+        LOOKLOCK
+    }
+}
