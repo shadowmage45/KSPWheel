ae4c767c5ca134a29efd704052fe52ffb600d138
shadowmage45 <shadowmage45@hotmail.com>
2017-02-15 15:13:33 -0700

    more work on new ALG module cleanup.  Most features should be back in place but still has some rigging stuff needing to be handled regarding deployment

--- VSProject/KSPWheel/PartModules/KSPWheelAdjustableGear2.cs
+++ VSProject/KSPWheel/PartModules/KSPWheelAdjustableGear2.cs
@@ -9,6 +9,8 @@ namespace KSPWheel
     public class KSPWheelAdjustableGear2 : KSPWheelSubmodule
     {
 
+        #region REGION - Standard Part Config File Fields
+
         [KSPField]
         public string suspensionContainer1Name = string.Empty;
 
@@ -51,33 +53,45 @@ namespace KSPWheel
         [KSPField]
         public bool canFlip = false;
 
-        [KSPField]
-        public float minExtension = 0.25f;
-
+        /// <summary>
+        /// User-selectable strut-extension.  Makes the entire gear 'longer'.
+        /// </summary>
         [KSPField]
         public float maxExtension = 0.5f;
 
+        /// <summary>
+        /// User-selectable strut angle min/max (negative value used for min)
+        /// </summary>
         [KSPField]
-        public float minSuspensionLength = 0.25f;
+        public float maxStrutAngle = 30f;
 
+        /// <summary>
+        /// User-selectable maximum wheel angle.  Minimum = 0
+        /// </summary>
         [KSPField]
-        public float maxSuspensionLength = 0.25f;
+        public float maxWheelAngle = 60f;
 
-        [KSPField]
-        public float minAngle = 0f;
-
-        [KSPField]
-        public float maxAngle = 30f;
-
+        /// <summary>
+        /// Angular rotation of the main strut during the retract animation;  this is around the horizontal (X) axis, and rotates the main strut upwards into the housing.
+        /// </summary>
         [KSPField]
         public float mainStrutRetractedAngle = 90f;
 
+        /// <summary>
+        /// Angular rotation of the secondary strut during retract animation; this is around the vertical (Y) axis, and rotates the wheel into the housing.
+        /// </summary>
         [KSPField]
         public float secStrutRetractedAngle = 90f;
 
+        /// <summary>
+        /// Angular rotation of the wheel housing during retract animation.  TODO
+        /// </summary>
         [KSPField]
         public float wheelHousingRetractedAngle = 0f;
 
+        /// <summary>
+        /// Angular rotation of the wheel bogey during retract animation.  TODO
+        /// </summary>
         [KSPField]
         public float wheelBogeyRetractedAngle = 0f;
 
@@ -89,32 +103,55 @@ namespace KSPWheel
         public float strutRotation = 0f;
 
         /// <summary>
-        /// Calculated secondary angle for wheel container to keep aligned with ground
+        /// User-set secondary angle for wheel container.  Determines the axis along which the suspension operates.
         /// </summary>
         [KSPField(guiName = "Wheel Angle", guiActive = false, guiActiveEditor = true, isPersistant = true),
          UI_FloatRange(minValue = 0f, maxValue = 60f, stepIncrement = 0.1f, suppressEditorShipModified = true)]
         public float wheelRotation = 0f;
 
+        /// <summary>
+        /// User-set strut extension value.  Makes the landing leg longer or shorter.  Does not effect suspension travel range.
+        /// </summary>
+        [KSPField(guiName = "Strut Extension", guiActive = false, guiActiveEditor = true, isPersistant = true),
+         UI_FloatRange(minValue = 0f, maxValue = 1, stepIncrement = 0.05f, suppressEditorShipModified = true)]
+        public float strutExtension = 0f;
+
+        /// <summary>
+        /// Temporary testing compression value -- TODO remove once module is finished being developed
+        /// </summary>
         [KSPField(guiName = "Comp Test", guiActive = false, guiActiveEditor = true, isPersistant = true),
          UI_FloatRange(minValue = 0f, maxValue = 1f, stepIncrement = 0.05f, suppressEditorShipModified = true)]
         public float compTest = 1f;
 
+        /// <summary>
+        /// Current animation state/time.  Stored independently of animation state (which is stored in the base module)
+        /// </summary>
         [KSPField(isPersistant = true)]
         public float animationTime = 0f;
 
+        /// <summary>
+        /// The speed of the animation. 1 = 1 second.  0.25 = 4 seconds. 0 = non-animated (infinite animation time). Lower values decrease playback speed; higher values increase it.
+        /// </summary>
         [KSPField]
-        public float animationSpeed = 1f;
+        public float animationSpeed = 0.25f;
 
         /// <summary>
-        /// Has user selected flipped wheel or set automatically from cloned state.  Determines wheel angle offset direction, wheel spin direction, others...
+        /// Has user selected flipped wheel or set automatically from cloned state.  Determines wheel angle offset direction, wheel spin direction, door opening directions, others...
         /// </summary>
         [KSPField(isPersistant = true)]
         public bool isFlipped = false;
 
+        #endregion ENDREGION - Standard Part Config File Fields
+
+        #region REGION - Private Working Variables
+
+        /// <summary>
+        /// Cached transforms for manipulation of the model
+        /// </summary>
         private Transform suspensionContainer1;
         private Transform suspensionContainer2;
         private Transform suspensionTarget;
-        private Transform wheelContainer;//used to position wheel collider according to model setup -AND- to rotate wheel for 'flip'
+        private Transform wheelContainer;
         private Transform mainStrut;
         private Transform secStrut;
         private Transform wheelHousing;
@@ -126,7 +163,9 @@ namespace KSPWheel
         private Transform rearDoorFlip;
 
         /// <summary>
+        /// Cached default orientations and locations for the above transforms
         /// Serialize these fields across parts (prefab -> editor; editor -> cloned)
+        /// Fixes problems of cloned models taking on new default orientations/locations from the part they were cloned from
         /// </summary>
         [SerializeField]
         private bool initializedDefaultRotations;
@@ -154,28 +193,72 @@ namespace KSPWheel
         [SerializeField]
         private Quaternion rearDoorFlipDefaultRotation;
 
+        #endregion ENDREGION - Private Working Variables
+
+        #region REGION - GUI methods
+
+        [KSPAction(actionGroup = KSPActionGroup.Gear, guiName = "Toggle Gear", requireFullControl = false)]
+        public void deployAction(KSPActionParam param)
+        {
+            if (controller == null)
+            {
+                return;//unpossible
+            }
+            if (param.type == KSPActionType.Activate)
+            {
+                switch (controller.wheelState)
+                {
+                    case KSPWheelState.RETRACTED:
+                        changeWheelState(KSPWheelState.DEPLOYING);
+                        break;
+                    case KSPWheelState.RETRACTING:
+                        changeWheelState(KSPWheelState.DEPLOYING);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else//if param.type==KSPActionType.Deactivate
+            {
+                switch (controller.wheelState)
+                {
+                    case KSPWheelState.DEPLOYED:
+                        changeWheelState(KSPWheelState.RETRACTING);
+                        break;
+                    case KSPWheelState.DEPLOYING:
+                        changeWheelState(KSPWheelState.RETRACTING);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
         [KSPEvent(guiName = "Toggle Gear", guiActive = true, guiActiveEditor = true)]
         public void deploy()
         {
-            switch (controller.wheelState)
+            this.symmetryUpdate(m =>
             {
-                case KSPWheelState.RETRACTED:
-                    changeWheelState(KSPWheelState.DEPLOYING);
-                    break;
-                case KSPWheelState.RETRACTING:
-                    changeWheelState(KSPWheelState.DEPLOYING);
-                    break;
-                case KSPWheelState.DEPLOYED:
-                    changeWheelState(KSPWheelState.RETRACTING);
-                    break;
-                case KSPWheelState.DEPLOYING:
-                    changeWheelState(KSPWheelState.RETRACTING);
-                    break;
-                case KSPWheelState.BROKEN:
-                    break;
-                default:
-                    break;
-            }
+                switch (m.controller.wheelState)
+                {
+                    case KSPWheelState.RETRACTED:
+                        m.changeWheelState(KSPWheelState.DEPLOYING);
+                        break;
+                    case KSPWheelState.RETRACTING:
+                        m.changeWheelState(KSPWheelState.DEPLOYING);
+                        break;
+                    case KSPWheelState.DEPLOYED:
+                        m.changeWheelState(KSPWheelState.RETRACTING);
+                        break;
+                    case KSPWheelState.DEPLOYING:
+                        m.changeWheelState(KSPWheelState.RETRACTING);
+                        break;
+                    case KSPWheelState.BROKEN:
+                        break;
+                    default:
+                        break;
+                }
+            });
         }
 
         [KSPEvent(guiName = "Flip Gear", guiActive = false, guiActiveEditor = true)]
@@ -196,16 +279,18 @@ namespace KSPWheel
         {
             this.symmetryUpdate(m =>
             {
-                //Vector3 target = Vector3.up + m.carriageTransform.position;//one unit above the transform, in world-space in the editor
-                //Vector3 localTarget = m.carriageTransform.InverseTransformPoint(target);//one unit above the transform, as seen in local space
-                ////rotating around the local Z axis, so we only care about the x and y offsets
-                ////erm.. feed this into Mathf.Atan2 as a slope, to get the returned angle
-                //float angle = -Mathf.Atan2(localTarget.x, localTarget.y) * Mathf.Rad2Deg;
-                //m.wheelRotation = Mathf.Clamp(m.wheelRotation + angle, -m.wheelRotationMax, m.wheelRotationMax);//clamp it to the current wheel angle limits
-                //m.carriageTransform.localRotation = m.wheelDefaultRotation;
-                //m.carriageTransform.Rotate(0, 0, m.wheelRotation, Space.Self);
+                Vector3 target = m.wheelContainer.position + Vector3.up;//one unit above the transform, in world-space in the editor
+                Vector3 localTarget = m.wheelContainer.InverseTransformPoint(target);//one unit above the transform, as seen in local space
+                //rotating around the local Z axis, so we only care about the x and y offsets
+                //erm.. feed this into Mathf.Atan2 as a slope, to get the returned angle
+                float angle = -Mathf.Atan2(localTarget.x, localTarget.y) * Mathf.Rad2Deg;
+                m.wheelRotation = Mathf.Clamp(m.wheelRotation + angle, 0, m.maxWheelAngle);//clamp it to the current wheel angle limits
             });
         }
+
+        #endregion ENDREGION - GUI Methods
+
+        #region REGION - Standard KSP/Unity Overrides
 
         public override void OnStart(StartState state)
         {
@@ -217,8 +302,9 @@ namespace KSPWheel
                 //the 'symmetry counterpart' that exists should be the original part
                 this.isFlipped = !part.symmetryCounterparts[0].GetComponent<KSPWheelAdjustableGear2>().isFlipped;
             }
-            updateAnimation(1.0f);
+            updateAnimation(0f);//force update the animation based on current time
         }
+
 
         internal override void preWheelFrameUpdate()
         {
@@ -227,11 +313,14 @@ namespace KSPWheel
             else if (controller.wheelState == KSPWheelState.RETRACTING) { updateAnimation(Time.deltaTime * -animationSpeed); }
             else if (controller.wheelState == KSPWheelState.DEPLOYED)
             {
-                updateAnimation(0f);//TODO....
-                //TODO update wheel mesh rotation based on wheel rpm and flipped state
-                //TODO update suspension transform layout based on wheel collider compression data
+                updateAnimation(0f);//TODO -- clean this up; only adjust suspension mesh stuff during normal deployed operation
+                wheelMesh.Rotate(0, wheel.perFrameRotation, 0, Space.Self);
             }
         }
+
+        #endregion ENDREGION - Standard KSP/Unity Overrides
+
+        #region REGION - Custom Update Methods
 
         /// <summary>
         /// Locates all of the relevant transforms for this module, and sets up their default orientation/location cached values
@@ -426,6 +515,7 @@ namespace KSPWheel
             else //transforms managed by deploy animation
             {
                 wheelContainer.Rotate(0, 0, wheelAngleRot, Space.Self);
+                //TODO -- this actually needs to lerp from strut to wheel-container positions based on the susTargetPos...
                 suspensionTarget.position = secStrut.position - secStrut.up * (wheel.length * susTargetPos);
                 mainStrut.Rotate(mainStrutRot, 0, 0, Space.Self);
                 secStrut.Rotate(0, secStrutRot, 0, Space.Self);
@@ -444,6 +534,8 @@ namespace KSPWheel
             float t = time - pStart;
             return p <= 0 ? 0 : t / p;
         }
+
+        #endregion ENDREGION - Custom Update Methods
 
     }
 }
