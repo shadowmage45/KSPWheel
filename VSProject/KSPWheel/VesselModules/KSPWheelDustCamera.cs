456f5380cd1b9fc452670d33cb4bfdd563d72d44
Shadowmage <shadowmage45@hotmail.com>
2018-03-20 15:59:21 -0600

    initial prep of rewrite of dust effects system

--- VSProject/KSPWheel/VesselModules/KSPWheelDustCamera.cs
+++ VSProject/KSPWheel/VesselModules/KSPWheelDustCamera.cs
@@ -186,4 +186,104 @@ namespace KSPWheel
         }
 
     }
+
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class DustColors : MonoBehaviour
+    {
+        private static Dictionary<String, BodyDustColors> bodyDustColors = new Dictionary<string, BodyDustColors>();
+        public static readonly Color defaultColor = new Color(0.75f, 0.75f, 0.75f, 0.014f);
+
+        public void Start()
+        {
+            GameObject.DontDestroyOnLoad(this);
+            GameEvents.OnGameDatabaseLoaded.Add(new EventVoid.OnEvent(gameDatabaseLoaded));
+        }
+
+        public void OnDestroy()
+        {
+            GameEvents.OnGameDatabaseLoaded.Remove(new EventVoid.OnEvent(gameDatabaseLoaded));
+        }
+
+        private void gameDatabaseLoaded()
+        {
+            loadDustColors();
+        }
+
+        public void ModuleManagerPostLoad()
+        {
+            loadDustColors();
+        }
+
+        public static Color getBodyColor(string body, string biome)
+        {
+            if (bodyDustColors.ContainsKey(body))
+            {
+                return bodyDustColors[body].getBiomeColor(biome);
+            }
+            return defaultColor;
+        }
+
+        public static void loadDustColors()
+        {
+            //TODO -- when would loading of the config values be appropriate outside of a module-manager context?
+            //TODO -- can only really load them on game-database finished processing
+            //TODO -- add a MM callback for -re-loading- of the color values
+            bodyDustColors.Clear();
+            ConfigNode[] potentialColorNodes = GameDatabase.Instance.GetConfigNodes("DustColorDefinitions");
+            if (potentialColorNodes == null || potentialColorNodes.Length <= 0) { return; }
+            ConfigNode bodyDustColorNode = potentialColorNodes[0];
+            ConfigNode bodyNode;
+            ConfigNode biomeNode;
+            String bodyName, biomeName;
+            Color color;
+            int len = bodyDustColorNode.nodes.Count;
+            for (int i = 0; i < len; i++)
+            {
+                bodyNode = bodyDustColorNode.nodes[i];
+                bodyName = bodyNode.name;
+                int len2 = bodyNode.nodes.Count;
+                for (int k = 0; k < len2; k++)
+                {
+                    biomeNode = bodyNode.nodes[k];
+                    biomeName = biomeNode.name;
+                    color = ConfigNode.ParseColor(biomeNode.GetStringValue("Color", "0.75, 0.75, 0.75, 0.014"));
+                    loadColor(bodyName, biomeName, color);
+                }
+            }
+        }
+
+        private static void loadColor(string body, string biome, Color color)
+        {
+            if (!bodyDustColors.ContainsKey(body))
+            {
+                bodyDustColors.Add(body, new BodyDustColors());
+            }
+            bodyDustColors[body].addColor(biome, color);
+        }
+    }
+
+    public class BodyDustColors
+    {
+        private Dictionary<String, Color> biomeDustColors = new Dictionary<string, Color>();
+        private Color bodyDefaultColor;
+
+        public Color getBiomeColor(string name)
+        {
+            if (biomeDustColors.ContainsKey(name)) { return biomeDustColors[name]; }
+            return bodyDefaultColor == null ? DustColors.defaultColor : bodyDefaultColor;
+        }
+
+        public void addColor(string biomeName, Color color)
+        {
+            if (biomeName.ToLower().Equals("default"))
+            {
+                bodyDefaultColor = color;
+                return;
+            }
+            if (!biomeDustColors.ContainsKey(biomeName))
+            {
+                biomeDustColors.Add(biomeName, color);
+            }
+        }
+    }
 }
